4617d92e4283c700714a3c234f4c9d45e40f55a7
hssen <hssengul92@gmail.com>
2026-02-10 22:54:52 +0300

    Force dynamic admin layout to avoid prerender

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,6 +2,8 @@ import type { ReactNode } from "react";
 import AdminGate from "../../app/components/admin/AdminGate";
 import AdminShell from "../../app/components/admin/AdminShell";
 
+export const dynamic = "force-dynamic";
+
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
     <AdminGate>
